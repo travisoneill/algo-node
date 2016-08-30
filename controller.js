9a855b4773b978f28aa4147e34d3326d3ce0df92
travis.h.oneill@gmail.com <travis.h.oneill@gmail.com>
2016-08-30 14:27:15 -0700

    comment out vm

--- controller.js
+++ controller.js
@@ -1,15 +1,7 @@
 'use strict';
-const VM = require('./vm');
+// const VM = require('./vm');
 
 module.exports = {
-  // receiveCode(codeObj) {
-  //   let lengthArr = codeObj.lengthArr;
-  //   let method = codeObj.algos.method1;
-  //   let method2 = codeObj.algos.method2;
-  //   let name = codeObj.algos.name1;
-  //   let methodTwoName = codeObj.algos.name2;
-  //   return this.testCode(method1, methodOneName, method2, methodTwoName, lengthArr);
-  // },
   receiveCode(codeObj) {
     const lengthArr = codeObj.lengthArr;
     const requestData = codeObj.request_data;
@@ -25,7 +17,7 @@ module.exports = {
     for (let i = 0; i < lengthArr.length; i++) {
       try {
           let n = lengthArr[i];
-          let res = VM.bootVM(method, name, n);
+          // let res = VM.bootVM(method, name, n);
           results.push({x: n, y: res});
           if(res > 20000){ break; }
       } catch(e) {
@@ -40,40 +32,4 @@ module.exports = {
     };
   },
 
-  // testCode(method1, name, method2, methodTwoName, lengthArr) {
-  //   let results1 = [];
-  //   let results2 = [];
-  //   let res1;
-  //   let res2;
-  //
-  //   for (let i = 0; i < lengthArr.length; i++) {
-  //     try {
-  //         let n = lengthArr[i];
-  //         let res1 = VM.bootVM(method1, name, n);
-  //         results1.push({x: n, y: res1});
-  //         if(res1 > 20000){ break; }
-  //     } catch(e) {
-  //         break;
-  //     }
-  //   }
-  //
-  //   for (let i = 0; i < lengthArr.length; i++) {
-  //     try {
-  //         let n = lengthArr[i];
-  //         let res2 = VM.bootVM(method2, methodTwoName, n);
-  //         results2.push({x: n, y: res2});
-  //         if(res2 > 20000){ break; }
-  //     } catch(e) {
-  //         break;
-  //     }
-  //   }
-  //
-  //   return {
-  //     xAxis: lengthArr,
-  //     rawData1: results1,
-  //     rawData2: results2,
-  //     name1: name,
-  //     name2: methodTwoName
-  //   };
-  // },
 };
